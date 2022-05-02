01380bd0c89486a05e4348047e0036795f0716bc
Andrey Mikhedko <mikhedko.a@gmail.com>
2022-05-02 21:08:48 +0300

    init: add Instabot project

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    slides: [
+      {
+        html: "1",
+      },
+      {
+        html: "2",
+      },
+      {
+        html: "3",
+      },
+    ],
+  });
